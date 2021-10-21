544c22394fce8b5873866a34c08e49f6eeed7d9e
pdevera15 <princedevera10@yahoo.com>
2021-10-21 19:36:31 +0900

    refactor

--- routes/post.ts
+++ routes/post.ts
@@ -14,80 +14,63 @@ type Topic = {
   author_id: string
 }
 
-router.post(
-  "/create_topic",
-  authenticateToken,
-  (req: Request, res: Response) => {
-    if (req.body) {
-      const title = req.body.title
-      const context = req.body.context
-      const date_created = new Date()
-      const date_updated = new Date()
-      const author_id = req.body.author
-      TopicRepo.AddTopic({
+/**
+ * Create new topic
+ * @param title, author_id, context
+ */
+router.post("/topics", authenticateToken, (req: Request, res: Response) => {
+  if (req.body) {
+    const { title, context, author: author_id } = req.body
+    const date_created = new Date()
+    const date_updated = new Date()
+    TopicRepo.AddTopic({
+      title,
+      context,
+      date_created,
+      date_updated,
+      author_id,
+    }).then((response) => {
+      response.acknowledged
+        ? res.json(ResponseMessage.POST_SUCCESS)
+        : res.json(ResponseMessage.POST_FAILED)
+    })
+  }
+})
+
+/**
+ * Update Topic
+ */
+router.put("/topics", authenticateToken, (req: Request, res: Response) => {
+  if (req.body) {
+    const { _id, title, context, author: author_id } = req.body
+    const date_updated = new Date()
+    TopicRepo.UpdateTopic(
+      { _id: new ObjectId(_id) },
+      {
         title,
         context,
-        date_created,
         date_updated,
         author_id,
-      }).then((response) => {
-        response.acknowledged
-          ? res.json(ResponseMessage.POST_SUCCESS)
-          : res.json(ResponseMessage.POST_FAILED)
-      })
-    }
+      }
+    ).then((response) => {
+      response.acknowledged
+        ? res.json(ResponseMessage.POST_SUCCESS)
+        : res.json(ResponseMessage.POST_FAILED)
+    })
   }
-)
+})
 
-router.post(
-  "/update_topic",
-  authenticateToken,
-  (req: Request, res: Response) => {
-    if (req.body) {
-      const _id = req.body._id
-      const title = req.body.title
-      const context = req.body.context
-      const date_updated = new Date()
-      const author_id = req.body.author
-      TopicRepo.UpdateTopic(
-        { _id: new ObjectId(_id) },
-        {
-          title,
-          context,
-          date_updated,
-          author_id,
-        }
-      ).then((response) => {
-        response.acknowledged
-          ? res.json(ResponseMessage.POST_SUCCESS)
-          : res.json(ResponseMessage.POST_FAILED)
-      })
-    }
-  }
-)
-
-router.get("/get_topics", (req: Request, res: Response) => {
+/**
+ * Find all topic
+ */
+router.post("/topics", authenticateToken, (req: Request, res: Response) => {
   if (req.body) {
-    const _id = req.body._id
-    const title = req.body.title
-    const context = req.body.context
+    const { _id, title, context, author_id: author } = req.body
     const date_updated = new Date()
-    const author_id = req.body.author
     TopicRepo.FindAllTopics().then((response) => {
       res.json(response)
     })
   }
 })
 
-router.post("/get_topics", authenticateToken, (req: Request, res: Response) => {
-  if (req.body) {
-    const _id = req.body._id
-    const title = req.body.title
-    const context = req.body.context
-    const date_updated = new Date()
-    const author_id = req.body.author
-    TopicRepo.FindAllTopics().then((response) => {
-      res.json(response)
-    })
-  }
-})
+export default router
